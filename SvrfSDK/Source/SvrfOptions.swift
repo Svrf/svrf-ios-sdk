feb97edcda1c276722a6cc0db9f6639320b88215
Brent Chow <brent@svrf.com>
2019-04-08 14:46:45 -0400

    Add missing parameters

--- SvrfSDK/Source/SvrfOptions.swift
+++ SvrfSDK/Source/SvrfOptions.swift
@@ -16,6 +16,10 @@ import SVRFClient
  - stereoscopicType: Search only for *Media* with a particular stereoscopic type.
  - category: Search only for *Media* with a particular category.
  - size: The number of results to return per-page, from 1 to 100.
+ - minimumWidth: The minimum width for video and photo Media, in pixels.
+ - isFaceFilter: Search only for face filters.
+ - hasBlendShapes: Search only for Media that has blend shapes.
+ - requiresBlendShapes: Search only for Media that requires blend shapes.
  - pageNum: Pagination control to fetch the next page of results, if applicable.
  */
 public struct SearchOptions {
@@ -57,6 +61,10 @@ public struct SearchOptions {
  - stereoscopicType: Search only for *Media* with a particular stereoscopic type.
  - category: Search only for *Media* with a particular category.
  - size: The number of results to return per-page, from 1 to 100.
+ - minimumWidth: The minimum width for video and photo Media, in pixels.
+ - isFaceFilter: Search only for face filters.
+ - hasBlendShapes: Search only for Media that has blend shapes.
+ - requiresBlendShapes: Search only for Media that requires blend shapes.
  - nextPageCursor: Pass this cursor ID to get the next page of results.
  */
 public struct TrendingOptions {
